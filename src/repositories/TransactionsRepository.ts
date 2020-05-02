1ac1bfe6c202f7bd6b6f38f8fa99974a6bc4dedc
Júlio César <juliocholiveira@gmail.com>
2020-05-02 08:14:58 -0300

    challenge finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+
+    const income = this.transactions.reduce((p, c) => {
+      let sum = p;
+      if (c.type === 'income') {
+        sum = p + c.value;
+      }
+      return sum;
+    }, 0);
+
+    const outcome = this.transactions.reduce((p, c) => {
+      let sum = p;
+      if (c.type === 'outcome') {
+        sum = p + c.value;
+      }
+      return sum;
+    }, 0);
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
